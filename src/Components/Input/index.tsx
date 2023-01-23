251494e32a8a6250f29c3057dd4c0b22ed701e94
Flavio Barbosa <flavio.barbosa@dataprev.gov.br>
2023-01-23 10:15:36 -0300

    fix input component

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { InputHTMLAttributes } from 'react';
 
 import { InputText } from 'primereact/inputtext';
 
-interface InputProps extends React.ComponentProps<'input'> {
+interface InputProps extends InputHTMLAttributes< HTMLInputElement> {
   id: string;
   label?: string;
   placeholder?: string;
   icon?: string;
   type?: string;
+
 }
 
-export function Input({id, label, placeholder, icon, type}: InputProps) {
+export function Input({id, label, placeholder, icon, type, ...rest}: InputProps) {
 
   return (
     <div>
@@ -20,12 +21,12 @@ export function Input({id, label, placeholder, icon, type}: InputProps) {
         (
           <span className="p-input-icon-left w-full">
             <i className={`pi pi-${icon}`} />
-            <InputText id={id} type={type ? type : 'text'} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full"  />
+            <InputText id={id} type={type ? type : 'text'} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full"  {...rest}  />
           </span>
         )
         :
         (
-          <InputText id={id} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full"/>
+          <InputText id={id} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full" {...rest} />
         )
       }
     </div>
