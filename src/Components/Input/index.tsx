fb1c52defacfb583c3505dbabb910479d8cfbbba
Flavio Barbosa <flavio.barbosa@dataprev.gov.br>
2023-01-23 09:59:22 -0300

    fix input component

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -10,7 +10,7 @@ interface InputProps extends React.ComponentProps<'input'> {
   type?: string;
 }
 
-export function Input({id, label, placeholder, icon, type, ...rest}: InputProps) {
+export function Input({id, label, placeholder, icon, type}: InputProps) {
 
   return (
     <div>
@@ -20,12 +20,12 @@ export function Input({id, label, placeholder, icon, type, ...rest}: InputProps)
         (
           <span className="p-input-icon-left w-full">
             <i className={`pi pi-${icon}`} />
-            <InputText id={id} type={type ? type : 'text'} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full" {...rest} />
+            <InputText id={id} type={type ? type : 'text'} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full"  />
           </span>
         )
         :
         (
-          <InputText id={id} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full" {...rest} />
+          <InputText id={id} placeholder={placeholder} aria-describedby={`${id}-help`} className="block w-full"/>
         )
       }
     </div>
